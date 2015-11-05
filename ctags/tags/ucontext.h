4b6365c8960f0f83739f8fd8c474be42e442fd6d
scps950707 <scps950707@gmail.com>
2015-11-06 01:49:45 +0800

    update

--- ctags/tags/ucontext.h
+++
@@ -1,8 +0,0 @@
-!_TAG_FILE_FORMAT	2	/extended format; --format=1 will not append ;" to lines/
-!_TAG_FILE_SORTED	1	/0=unsorted, 1=sorted, 2=foldcase/
-!_TAG_PROGRAM_AUTHOR	Darren Hiebert	/dhiebert@users.sourceforge.net/
-!_TAG_PROGRAM_NAME	Exuberant Ctags	//
-!_TAG_PROGRAM_URL	http://ctags.sourceforge.net	/official site/
-!_TAG_PROGRAM_VERSION	5.9~svn20110310	//
-_UCONTEXT_H	/usr/include/ucontext.h	21;"	d
-makecontext	/usr/include/ucontext.h	/^extern void makecontext (ucontext_t *__ucp, void (*__func) (void),$/;"	p	signature:(ucontext_t *__ucp, void (*__func) (void), int __argc, ...)
